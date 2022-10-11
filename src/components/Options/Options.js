7666eaf4a5bcf57d9829ac3056c0a00d87f708b2
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-11 15:57:40 +0600

    Option added

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Options = ({ option }) => {
+    console.log(option);
     return (
         <div>
-            <button type='radio' ><p>{option}</p></button>
+            <button type='radio' className=' w-full bg-gray-50 m-1 text-gray-600 p-1 rounded shadow' >  <p>{option}</p> </button>
         </div>
     );
 };
