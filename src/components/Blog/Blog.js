d8c703fd225604e9e4860952ba72d4b7781eeae6
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-12 13:04:49 +0600

    Blog added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,25 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1>This is from Brog</h1>
+            <h1 className="text-2xl text-slate-500 font-semibold">Frequently Asked Questions</h1>
+            <div className='w-3/4  mx-auto rounded shadow-lg p-6 my-6 text-start'>
+                <h1 className="text-xl text-slate-600 font-semibold">1. What is the purpose of react router?</h1>
+                <p className="text-lg text-slate-600 font-semibold">Answer: ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
+            </div>
+            <div className='w-3/4  mx-auto rounded shadow-lg p-6 my-6 text-start'>
+                <h1 className="text-xl text-slate-600 font-semibold">2. How does contex API works?</h1>
+                <p className="text-lg text-slate-600 font-semibold">Answer: The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management.
+                    In a typical React application, data is passed top-down (parent to child) via props, but such usage can be cumbersome for certain types of props (e.g. locale preference, UI theme) that are required by many components within an application. Context provides a way to share values like these between components without having to explicitly pass a prop through every level of the tree.</p>
+            </div>
+            <div className='w-3/4  mx-auto rounded shadow-lg p-6 my-6 text-start'>
+                <h1 className="text-xl text-slate-600 font-semibold">3. Tell something about useRef? </h1>
+                <p className="text-lg text-slate-600 font-semibold">Answer:
+                    <li>The useRef Hook allows you to persist values between renders.</li>
+                    <li>It can be used to store a mutable value that does not cause a re-render when updated.</li>
+                    <li>It can be used to access a DOM element directly.</li>
+                    <li>If we tried to count how many times our application renders using the useState Hook, we would be caught in an infinite loop since this Hook itself causes a re-render. To avoid this, we can use the useRef Hook.</li>
+                </p>
+            </div>
         </div>
     );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Bar, BarChart, CartesianGrid, ComposedChart, Funnel, FunnelChart, LabelList, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, ComposedChart, Funnel, FunnelChart, LabelList, Legend, Line, LineChart, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import { TopicContext } from '../Main/Main';
 
 const Statistics = () => {
@@ -9,73 +9,83 @@ const Statistics = () => {
         <div className='w-11/12 mx-auto'>
             <h1 className="text-2xl text-slate-500 font-semibold">We have some statistics</h1>
             <h2 className="text-xl text-red-500 font-semibold">Total Quiz VS Topic Name</h2>
-            <div className='grid md:grid-cols-2  mt-9 gap-2'>
-                <ComposedChart
-                    width={350}
-                    height={300}
-                    data={topics}
-                    margin={{
-                        top: 20,
-                        right: 20,
-                        bottom: 20,
-                        left: 20,
-                    }}
-                >
-                    <CartesianGrid stroke="#f5f5f5" />
-                    <XAxis dataKey="name" scale="band" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="total" barSize={20} fill="#413ea0" />
-                    <Line type="monotone" dataKey="total" stroke="#ff7300" />
-                </ComposedChart>
-                <PieChart width={350} height={300}>
-                    <Pie
-                        dataKey="total"
-                        isAnimationActive={true}
+            <div className='grid md:grid-cols-2  mt-16 gap-2'>
+                <ResponsiveContainer width="95%" height={400}>
+                    <ComposedChart
+                        width={350}
+                        height={300}
                         data={topics}
-                        cx="50%"
-                        cy="50%"
-                        outerRadius={80}
-                        fill="#F90830"
-                        label
-                    />
-                    <Tooltip />
-                </PieChart>
-                <LineChart
-                    width={350}
-                    height={300}
-                    data={topics}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
-                </LineChart>
-                <BarChart width={350} height={300} data={topics}>
-                    <Tooltip></Tooltip>
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Bar dataKey="total" fill="#8884d8" />
-                </BarChart>
-                <FunnelChart width={350} height={300}>
-                    <Tooltip />
-                    <Funnel
-                        dataKey="total"
-                        data={topics}
-                        isAnimationActive
+                        margin={{
+                            top: 20,
+                            right: 20,
+                            bottom: 20,
+                            left: 20,
+                        }}
                     >
-                        <LabelList position="right" fill="#000" stroke="none" dataKey="name" />
-                    </Funnel>
-                </FunnelChart>
+                        <CartesianGrid stroke="#f5f5f5" />
+                        <XAxis dataKey="name" scale="band" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="total" barSize={20} fill="#413ea0" />
+                        <Line type="monotone" dataKey="total" stroke="#ff7300" />
+                    </ComposedChart>
+                </ResponsiveContainer>
+                <ResponsiveContainer width="95%" height={400}>
+                    <PieChart width={350} height={300}>
+                        <Pie
+                            dataKey="total"
+                            isAnimationActive={true}
+                            data={topics}
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={80}
+                            fill="#F90830"
+                            label
+                        />
+                        <Tooltip />
+                    </PieChart>
+                </ResponsiveContainer>
+                <ResponsiveContainer width="95%" height={400}>
+                    <LineChart
+                        width={350}
+                        height={300}
+                        data={topics}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    </LineChart>
+                </ResponsiveContainer>
+                <ResponsiveContainer width="95%" height={400}>
+                    <BarChart width={350} height={300} data={topics}>
+                        <Tooltip></Tooltip>
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Bar dataKey="total" fill="#8884d8" />
+                    </BarChart>
+                </ResponsiveContainer>
+                <ResponsiveContainer width="95%" height={400}>
+                    <FunnelChart width={350} height={300}>
+                        <Tooltip />
+                        <Funnel
+                            dataKey="total"
+                            data={topics}
+                            isAnimationActive
+                        >
+                            <LabelList position="right" fill="#000" stroke="none" dataKey="name" />
+                        </Funnel>
+                    </FunnelChart>
+                </ResponsiveContainer>
             </div>
         </div>
 
